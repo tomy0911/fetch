17880ceba6449445f3153afa352b978b3833c6c9
Tomas Moriondo <tomasmoriondo54321@gmail.com>
2022-07-12 13:48:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let url = "https://jsonplaceholder.typicode.com/users/";
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => mostrarData(data))
+  .catch((error) => console.log(error));
+
+const mostrarData = (data) => {
+  console.log(data);
+  let body = "";
+  for (var i = 0; i < data.length; i++) {
+    body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`;
+  }
+  document.getElementById("data").innerHTML = body;
+};
